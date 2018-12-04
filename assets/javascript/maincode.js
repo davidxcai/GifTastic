522f91ffd36c0f5cac1e3633739bebe137c2dfce
Dafydd <dafyddzephyr@gmail.com>
2018-12-03 21:58:47 -0800

    finished most of the code

--- assets/javascript/maincode.js
+++ assets/javascript/maincode.js
@@ -1,72 +1,113 @@
 $(document).ready()
 
-var gifButtons = [];
+var gifButtons = ["Pikachu", "Eevee", "Tommy Wiseau"];
 
-// var rating = $("#ratingSelect").attr("value");
+renderButtons();
 
-// var limit = $("#limitSelect option").click(function(e){
-//     $("#ratingSelect").text(this.innerHTML);
-// });
+//Sets default parameters on page load
+var rating = "G";
+//Grabs the value from ratings dropdown menu and stores it in var
+$("#ratingSelect").click(function() {
+    rating = $(this).val();
+});
+var limit = 5;
+//Grabs the value from limit dropdown menu and stores it in var
+$("#limitSelect").click(function() {
+    limit = $(this).val();
+});
 
-$(".gifBtn").on("click", function () {
+//Creates buttons when invoked
+function renderButtons() {
+    $("#buttonBody").empty();
+
+    for (var i = 0; i < gifButtons.length; i++) {
+
+        var newBtn = $("<button>")
+
+        newBtn.addClass("btn gifBtn");
+
+        newBtn.attr("value", gifButtons[i]);
+
+        newBtn.text(gifButtons[i]);
+
+        $("#buttonBody").append(newBtn);
+    }
+}
+
+//Add Catagory button function
+$("#addCat").click(function (event) {
+    console.log("hello");
+    event.preventDefault();
+    
+    var newGif = $("#searchBar").val().trim();
+    //If input field is blank, it will not create a button
+    if (newGif === "") {
+        console.log("Requires user input");
+    }
+    else {
+        gifButtons.push(newGif);
+    }
+    console.log("Input: " + newGif);
+    renderButtons();
+
+})
+
+//Displays gifs on page
+$(document).on("click", '.gifBtn', function () {
     $("#displayBody").empty();
 
     var gif = $(this).attr("value");
-
-    var limit = 5;
-    
-    var rating = "G";
-
-    // var rating = $("#ratingSelect option").attr("value");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gif + "&api_key=1R8LZbSSu8CX60HOGVtzyg5aPpyZxprl&limit=" + limit +
         "&rating=" + rating + "&lang=en"
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            console.log(response.data);
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+
+                var gifDiv = $("<div>");
+
+                var title = $("<h6>").text(results[i].title);
+                title.addClass("gif-title");
+
+                var R = $("<p>").text("Rating: " + (results[i].rating).toUpperCase());
+                R.addClass("gif-rating");
+
+                var gifImg = $("<img>");
+
+                var stillImage = results[i].images.fixed_height_still.url;
+
+                var animateImage = results[i].images.fixed_height.url;
+
+                gifImg.attr({
+                    src: stillImage,
+                    still: stillImage,
+                    animate: animateImage,
+                    state: "still",
+                    class: "gif"
+                });
+
+                gifDiv.append(gifImg, title, R);
+
+                $("#displayBody").append(gifDiv);
+
+                $("#gifDisplay").show();
+
+            }
         })
-            .then(function (response) {
-                console.log(queryURL);
-
-                console.log(response);
-
-                console.log("limit: " + limit);
-
-                console.log("rating: " + rating);
-
-                var results = response.data;
-
-                for (var i = 0; i < results.length; i++) {
-
-                    var gifDiv = $("<div>");
-
-                    var p = $("<p>").text("Rating: " + results[i].rating);
-
-                    var gifImg = $("<img>");
-
-                    var stillImage = results[i].images.fixed_height_still.url;
-
-                    var animateImage = results[i].images.fixed_height.url;
-
-                    gifImg.attr({
-                        src: stillImage,
-                        still: stillImage,
-                        animate: animateImage,
-                        state: "still",
-                        class: "gif"
-                    });
-
-                    gifDiv.append(gifImg, p);
-
-                    $("#displayBody").append(gifDiv);
-
-                }
-            })
 })
 
-$(document).on("click", '.gif', function(){
+//Looks for class "gif" and toggles still/animate function
+$(document).on("click", '.gif', function () {
 
     var state = $(this).attr("state");
 
